46382807e8f89c46753d864f43f0b9646da763c0
Matthew Peters <matthewpeters@MacBook-Pro-3.local>
2019-05-07 22:01:59 -0400

    initial version

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+  var firstDate = new Date(2019,5,18);
+  var secondDate = new Date();
+
+  var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+
+  var firstDate2 = new Date(2020,0,10);
+  console.log(firstDate2);
+
+  var diffDays2 = Math.round(Math.abs((firstDate2.getTime() - secondDate.getTime())/(oneDay)));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="parent">
+      <div class="row">
+        <div class="column">
+          <img src="https://4.bp.blogspot.com/-NpiejDEE5Ys/XD9B60tqp_I/AAAAAAAANSI/zKI_HZTE6oUSUeegMPYySwtLSLhhB-KGQCLcBGAs/s400/20190108_102351.jpg" alt="Xander"/>
+        </div>
+        <div class="column">
+          <h2>{diffDays} Days to Xander's Birthday</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column">
+        <img src="https://4.bp.blogspot.com/-RH3sCHvpgpI/W7FO3vUuE7I/AAAAAAAALbI/SPp8-p2G9xY-_HZ7xJVxNdK1GdFB-LraQCLcBGAs/s400/DSC_0440.jpg" alt="William"/>
+        </div>
+        <div class="column">
+          <h2>{diffDays2} Days to William's Birthday</h2>
+        </div>
+      </div>
     </div>
   );
 }
